fe889329a899909adcf0e3af6719ba18c14e213b
Volki312 <josip312@hotmail.com>
2020-02-22 23:01:26 +0100

    🎉Finish ActivityBar and StatusBar HTML+CSS

--- src/components/shared/ActivityBar/ActivityBar.jsx
+++ src/components/shared/ActivityBar/ActivityBar.jsx
@@ -6,14 +6,40 @@ import { faSearch, faCodeBranch as faSourceControl, faBug as faDebug, faPuzzlePi
 
 const ActivityBar = () => {
 	return (
-		<ul className="activity_bar">
-			<li className="activity_bar-item"><FontAwesomeIcon icon={faExplorer} /></li>
-			<li className="activity_bar-item"><FontAwesomeIcon icon={faSearch} /></li>
-			<li className="activity_bar-item"><FontAwesomeIcon icon={faSourceControl} /></li>
-			<li className="activity_bar-item"><FontAwesomeIcon icon={faDebug} /></li>
-			<li className="activity_bar-item"><FontAwesomeIcon icon={faExtensions} /></li>
-			<li className="activity_bar-item activity_bar-item--settings"><FontAwesomeIcon icon={faSettings} /></li>
-		</ul>
+		<aside className="aside">
+			<ul className="aside-list flex-column">
+				<li className="aside-item">
+					<button className="aside-button" autofocus="autofocus">
+						<FontAwesomeIcon className="aside-fa" icon={faExplorer} />
+					</button>
+				</li>
+				<li className="aside-item">
+					<button className="aside-button">
+						<FontAwesomeIcon className="aside-fa" icon={faSearch} />
+					</button>
+				</li>
+				<li className="aside-item">
+					<button className="aside-button">
+						<FontAwesomeIcon className="aside-fa" icon={faSourceControl} />
+					</button>
+				</li>
+				<li className="aside-item">
+					<button className="aside-button">
+						<FontAwesomeIcon className="aside-fa" icon={faDebug} />
+					</button>
+				</li>
+				<li className="aside-item">
+					<button className="aside-button">
+						<FontAwesomeIcon className="aside-fa" icon={faExtensions} />
+					</button>
+				</li>
+				<li className="aside-item aside-item--settings">
+					<button className="aside-button aside-button--settings">
+						<FontAwesomeIcon className="aside-fa" icon={faSettings} />
+					</button>
+				</li>
+			</ul>
+		</aside>
 	 );
 }
 
--- src/components/shared/StatusBar/StatusBar.jsx
+++ src/components/shared/StatusBar/StatusBar.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodeBranch as faSourceControl} from '@fortawesome/free-solid-svg-icons'
+import { faBell as faNotification, faThumbsUp} from '@fortawesome/free-regular-svg-icons'
+
 const StatusBar = () => {
 	return (
-		<footer>
-			StatusBar
+		<footer className="footer flex-row">
+			<div className="flex-column">
+				<span className="footer-span">
+					<FontAwesomeIcon className="footer-fa" icon={faSourceControl} />develop-branch
+				</span>
+			</div>
+
+			<div className="flex-column">
+				<span className="footer-span footer-span--small">
+					<small className="footer-small">USR - Registar udruga Republike Hrvatske broj 10932480923 od 21.6.2020. u Splitu. OIB: 63923092133</small>
+				</span>
+			</div>
+
+			<div className="flex-column">
+				<div className="flex-row">
+					<span className="footer-span">
+						<FontAwesomeIcon className="footer-fa" icon={faThumbsUp} />
+					</span>
+
+					<span className="footer-span">
+						<FontAwesomeIcon className="footer-fa" icon={faNotification} />
+					</span>
+				</div>
+			</div>
 		</footer>
 	 );
 }
